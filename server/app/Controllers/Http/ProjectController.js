ba802187534e041348e619e76bf72ff293c3eb3d
Domagoj Glavacevic <domagoj.glavacevic_extern@gauss.hr>
2019-03-14 21:44:18 +0100

    Patch Update

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -28,6 +28,15 @@ async destroy({ auth, request, params }) {
     await project.delete();
     return project;
 }
+async update({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    project.merge(request.only('title'));
+    await project.save();
+    return project;
+}
 }
 
 module.exports = ProjectController
--- server/start/routes.js
+++ server/start/routes.js
@@ -26,5 +26,7 @@ Route.group(() => {
   Route.get('projects', 'ProjectController.index').middleware('auth');
   Route.post('projects', 'ProjectController.create').middleware('auth');
   Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth');
+  Route.patch('projects/:id', 'ProjectController.update').middleware('auth');
+
 })
 .prefix('api');
