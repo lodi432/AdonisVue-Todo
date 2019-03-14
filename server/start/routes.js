2fe3ad3ce64e5061d5c9716b016fe0e02e7909bd
Domagoj Glavacevic <domagoj.glavacevic_extern@gauss.hr>
2019-03-14 23:27:29 +0100

    Task comit

--- server/start/routes.js
+++ server/start/routes.js
@@ -23,6 +23,7 @@ Route.get('/', () => {
 Route.group(() => {
   Route.post('auth/register', 'UserController.register');
   Route.post('auth/login', 'UserController.login');
+  
   Route.get('projects', 'ProjectController.index').middleware('auth');
   Route.post('projects', 'ProjectController.create').middleware('auth');
   Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth');
@@ -30,6 +31,8 @@ Route.group(() => {
 
   Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth');
   Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth');
+  Route.delete('tasks/:id', 'TaskController.destroy').middleware('auth');
+  Route.patch('tasks/:id', 'TaskController.update').middleware('auth');
 
 })
 .prefix('api');
